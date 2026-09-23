48c0b6662c0f348eef8664f78562f79768821d76
H0o0NsEOK <tjrgnsrla1427@gmail.com>
2026-09-23 10:32:44 +0900

    Delete repository directory

--- repository/SupplementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smhrd.repository;
-
-import com.smhrd.entity.Supplement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplementRepository
-        extends JpaRepository<Supplement, Long> {
-}
